f00c5c76f049833192f965660e03e5f3d76bb272
Flora Wang <florawang0611@gmail.com>
2021-11-30 12:37:26 -0800

    updated webapp

---
+++ webapp/app/__init__.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request
+# from .app import insert_message, random_messages
+
+app = Flask(__name__)
+
+@app.route("/")
+def main():
+    return render_template("home.html")
+
+@app.route("/about/")
+def about():
+    return render_template("about.html")
+    # if request.method == 'GET':
+    #     return render_template("submit.html")
+    # else:
+    #     insert_message(request)
+    #     return render_template("submit.html", thanks = True)
+
+@app.route("/more/")
+def more():
+    # msg = random_messages(5)
+    return render_template("more.html")
+
